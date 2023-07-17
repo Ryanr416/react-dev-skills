96987e7f4ce5cd8a3f20d98ab71619e632fb7439
ryanr416 <ryanrichardson0993@gmail.com>
2023-07-17 12:24:20 -0400

    added button

--- src/NewSkillForm.jsx
+++ src/NewSkillForm.jsx
@@ -6,6 +6,17 @@ export default function NewSkillForm() {
           Skill
           <input type="text" name="Skill" />{" "}
         </label>
+        <label>Level  
+            <select>
+                <option value="1"> 1</option>
+                <option value="2"> 2</option>
+                <option value="3"> 3</option>
+                <option value="4"> 4</option>
+                <option value="5"> 5</option>
+                
+            </select>
+            <button>Add Skill</button>
+        </label>
       </form>
     );
   }
